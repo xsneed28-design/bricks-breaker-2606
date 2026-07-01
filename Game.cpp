7e7abd995c3cd378452624db61a2fd2cbdfa9a0f
xsneed28-design <xsneed28@gmail.com>
2026-07-01 19:04:28 -0400

    Brick removal upon collision added, task 5 complete

--- Game.cpp
+++ Game.cpp
@@ -128,7 +128,9 @@ void Game::CheckCollision()
 			ball.y_velocity *= -1;
 
 			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-			
+			if (bricks[i].color = ConsoleColor(Black)) {
+				bricks.erase(bricks.begin() + i);
+			}
 		}
 	}
 
